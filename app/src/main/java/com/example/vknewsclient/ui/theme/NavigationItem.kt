6e8a0046a46d469141c376eccd56edc89625262f
zhanibekishimss <230103150@sdu.edu.kz>
2024-08-04 13:35:36 +0500

    fix bags

--- app/src/main/java/com/example/vknewsclient/ui/theme/NavigationItem.kt
+++ app/src/main/java/com/example/vknewsclient/ui/theme/NavigationItem.kt
@@ -6,9 +6,29 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.vknewsclient.R
+import com.example.vknewsclient.navigation.Screen
 
-sealed class NavigationItem(val title: Int,val icon: ImageVector) {
-    data object Favorite:NavigationItem(title = R.string.favorite_title, icon = Icons.Outlined.Favorite)
-    data object Profile:NavigationItem(title = R.string.profile_title, icon = Icons.Outlined.Person)
+sealed class NavigationItem(
+    val screen: Screen,
+    val titleResId: Int,
+    val icon: ImageVector
+) {
+
+    data object Home : NavigationItem(
+        screen = Screen.NewsFeed,
+        titleResId = R.string.home_title,
+        icon = Icons.Outlined.Home
+    )
+
+    data object Favourite : NavigationItem(
+        screen = Screen.Favourite,
+        titleResId = R.string.favorite_title,
+        icon = Icons.Outlined.Favorite
+    )
+
+    data object Profile : NavigationItem(
+        screen = Screen.Profile,
+        titleResId = R.string.profile_title,
+        icon = Icons.Outlined.Person
+    )
 }
-
--- app/src/main/java/com/example/vknewsclient/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/example/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -1,89 +1,84 @@
 package com.example.vknewsclient.ui.theme
 
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.foundation.clickable
+import androidx.compose.material.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import kotlinx.coroutines.launch
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.vknewsclient.MainViewModel
+import com.example.vknewsclient.navigation.AppNavGraph
+import com.example.vknewsclient.navigation.Screen
 
-
-@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class,
-    ExperimentalFoundationApi::class
-)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MainScreen(viewModel: MainViewModel){
-    val scope  = rememberCoroutineScope()
-    val snackBarState = SnackbarHostState()
-    val fabIsVisible = remember { mutableStateOf(true) }
-    Scaffold(snackbarHost = {
-        SnackbarHost(hostState = snackBarState)
-    }, floatingActionButton = {
-        if(fabIsVisible.value){
-            FloatingActionButton(onClick = {
-                scope.launch {
-                    val result = snackBarState.showSnackbar(
-                        message = "This is snacknbar",
-                        actionLabel = "Hide FAB",
-                        duration = SnackbarDuration.Long)
-                    if(result == SnackbarResult.ActionPerformed) {
-                        fabIsVisible.value = false
-                    }
-                }
-            }) {
-                Icon(Icons.Outlined.Favorite, contentDescription = null)
-            }
-        }
-    },bottomBar = {
-        NavigationBar {
-            val selectedItemPosition = remember{ mutableIntStateOf(0) }
-            val items = listOf(
-                NavigationItem.Favorite,
-                NavigationItem.Profile)
-                NavigationBarItem(
-                    icon = { Icon(item.icon, contentDescription = null) },
-                    label = { Text(text = stringResource(id = item.title)) },
-                    colors = NavigationBarItemDefaults.colors(
-                        selectedIconColor = MaterialTheme.colorScheme.onPrimary,
-                        selectedTextColor = MaterialTheme.colorScheme.onPrimary,
-                        unselectedIconColor =  MaterialTheme.colorScheme.onSecondary,
-                        unselectedTextColor =  MaterialTheme.colorScheme.onSecondary,
-                        indicatorColor =  Color.Cyan)
-                )
-            }
-        }
-    }) {contentPadding ->
-            }
+fun MainScreen(viewModel: MainViewModel) {
+    val navHostController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+                val currentRout = navBackStackEntry?.destination?.route
 
+                val items = listOf(
+                    NavigationItem.Home,
+                    NavigationItem.Favourite,
+                    NavigationItem.Profile
+                )
+                items.forEach { item ->
+                    BottomNavigationItem(
+                        selected = currentRout == item.screen.route,
+                        onClick = { navHostController.navigate(item.screen.route){
+                            popUpTo(Screen.NewsFeed.route) { saveState = true }
+                            launchSingleTop = true
+                            restoreState = true
+                        } },
+                        icon = {
+                            Icon(item.icon, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = stringResource(id = item.titleResId))
+                        },
+                        selectedContentColor = MaterialTheme.colorScheme.onPrimary,
+                        unselectedContentColor = MaterialTheme.colorScheme.onSecondary
+                    )
+                }
+            }
         }
+    ) { paddingValues ->
+        AppNavGraph(
+            navHostController = navHostController,
+            homeScreenContent = {
+                HomeScreen(
+                    viewModel = viewModel,
+                    paddingValues = paddingValues
+                )
+            },
+            favouriteScreenContent = { TextCounter(name = "Favourite") },
+            profileScreenContent = { TextCounter(name = "Profile") }
+        )
     }
 }
 
+@Composable
+private fun TextCounter(name: String) {
+    var count by remember {
+        mutableStateOf(0)
+    }
 
-
+    Text(
+        modifier = Modifier.clickable { count++ },
+        text = "$name Count: $count",
+        color = Color.Black
+    )
+}
 
 
 
